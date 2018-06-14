13e9622a93aa29ab91fdd5aada2f3b6f91b57d7b
Alain Van Driessche <alainvd@gmail.com>
2018-06-14 17:08:59 +0200

    Every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('cnect:scorecast')->everyFifteenMinutes();
+        $schedule->command('cnect:scorecast')->everyMinute();
     }
 
     /**
